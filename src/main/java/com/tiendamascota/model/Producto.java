19c04aadf2a8a88564560c827a46d31e590cd98f
JesusDev <ju.llontop@duocuc.cl>
2025-11-25 02:04:45 -0300

    feat: devolver 400 para validaciones y JSON inválido en GlobalExceptionHandler

--- src/main/java/com/tiendamascota/model/Producto.java
+++ src/main/java/com/tiendamascota/model/Producto.java
@@ -1,8 +1,8 @@
 package com.tiendamascota.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonAlias;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
